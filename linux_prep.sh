321e3f43f8746ed9a81bea2279abae76949bf00d
sgtslaughta <59345731+sgtslaughta@users.noreply.github.com>
2022-01-22 15:44:08 -0500

    Update linux_prep.sh

--- linux_prep.sh
+++ linux_prep.sh
@@ -4,6 +4,7 @@ NC='\033[0m' # No Color
 GREEN='\033[0;32m'
 CYAN='\033[1;36m'
 RED='\033[0;31m'
+YELLOW='\033[1;33m'
 
 # Add any apt based package installs under this function
 apt-install() {
@@ -28,11 +29,16 @@ pip-install() {
 # Install Docker and docker-compose
 docker-install() {
 	printf "\n${GREEN}[+]${NC} ${CYAN}INSTALLING DOCKER AND DOCKER-COMPOSE ${NC}\n\n"
+	FILE="/usr/share/keyrings/docker-archive-keyring.gpg"
 	apt install ca-certificates curl gnupg lsb-release -y
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-	apt update
-    apt install docker-ce docker-ce-cli containerd.io -y
+	if ! test -f "$FILE" ; then 
+		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o ${FILE}
+		echo "deb [arch=$(dpkg --print-architecture) signed-by=$FILE] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+		apt update
+	else
+		printf "\n${YELLOW}\t[-]${NC} Looks like the Docker GPG keys are present, attempting to update packages...\n"
+	fi
+    	apt install docker-ce docker-ce-cli containerd.io -y
 	apt install docker-compose -y
 }
 
@@ -41,12 +47,15 @@ pcte-prep() {
 	printf "\n${GREEN}[+]${NC} ${CYAN}RUNNING PCTE PREP ${NC}\n\n"
 	chmod +x "$FILE"
 	./$FILE
-	ln -s /opt/puppetlabs/bin/puppet /usr/bin/puppet
+	if ! test -f /usr/bin/puppet; then
+		printf "\n${YELLOW}\t[-]${NC} Creating sym link to puppet in /usr/bin...\n"
+		ln -s /opt/puppetlabs/bin/puppet /usr/bin/puppet
+	fi
 }
 
 # Set conky to autostart and move config to location if necessary
 conky-setup() {
-	printf "\n${GREEN}[+]${NC} ${CYAN}SETTING CONKY TO AUTOSTART AND COPYING FILES ${NC}\n\n"
+	printf "\n${GREEN}[+]${NC} ${CYAN}INSTALLING CONKY, COPYING FILES, AND SETTING TO AUTOSTART ${NC}\n\n"
 	CONFIG="lib/.conkyrc"
 	FILE=/home/$SUDO_USER/.config/autostart/conky.desktop
 	DIR=/home/$SUDO_USER/.config/autostart/
@@ -57,17 +66,20 @@ conky-setup() {
 			printf "\n${RED}Conky config missing, skipping...${NC}\n\n"
 		else
 			if ! test -f /home/$SUDO_USER/.conkyrc ; then
+				printf "\n${YELLOW}\t[-]${NC} Copying config file to home directory...\n"
 				cp $CONFIG /home/$SUDO_USER/.conkyrc
 			else
 				printf "\n${RED}Conky config already present in home directory, skipping...${NC}\n\n"
 			fi
 			if ! test -d "$DIR" ; then
+				printf "\n${YELLOW}\t[-]${NC} Creating autostart directory for USER: ${SUDO_USER}...\n"
 				mkdir -p $DIR
 				chown $SUDO_USER $DIR
 			else
 				printf "\n${RED}${DIR} already present, skipping create...${NC}\n\n"
 			fi
 			if ! test -f "$FILE" ; then
+				printf "\n${YELLOW}\t[-]${NC} Creating autostart entry for Conky...\n"
 				touch $FILE
 				chown $SUDO_USER $FILE
 				echo -e "[Desktop Entry]\nType=Application\nExec=conky\nHidden=false\nNoDisplay=false\nX-GNOME-Autostart-enabled=true\nName[en_US]=Conky\nName=Conky\nComment[en_US]=\nComment=" >> $FILE
